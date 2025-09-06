84a0e0e69176b0169f9788ff6bb03cf9d9fa3515
Francesco <frarampio@gmail.com>
2025-09-06 14:28:28 +0200

    Training model with different sizes

--- script/aia_2_stix_dm.sh
+++ script/aia_2_stix_dm.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --time=7-00:00:00
 #SBATCH --partition=performance
-#SBATCH --job-name="training_aia_2_stix_veryverysmall"
-#SBATCH --error=./logs/err/err_training_aia_2_stix_veryverysmall.log
-#SBATCH --out=./logs/out/out_training_aia_2_stix_veryverysmall.log
+#SBATCH --job-name="training_aia_2_stix_extra_small"
+#SBATCH --error=./logs/err/err_training_aia_2_stix_extra_small.log
+#SBATCH --out=./logs/out/out_training_aia_2_stix_extra_small.log
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 
@@ -13,10 +13,10 @@ epochs=1000
 batch_size=32
 evaluate_every=5
 save_every=5
-config_file="/mnt/nas05/data01/francesco/AIA2STIX/training/configs/very_very_small_model.json"
+config_file="/mnt/nas05/data01/francesco/AIA2STIX/training/configs/extra_small_model.json"
 saving_path="/mnt/nas05/data01/francesco/AIA2STIX/saved_models/"
-dir_name="aia_2_stix_very_very_small"
-wandb_run_name="aia_2_stix_very_very_small"
+dir_name="aia_2_stix_extra_small"
+wandb_run_name="aia_2_stix_extra_small"
 
 python /mnt/nas05/data01/francesco/AIA2STIX/training/train_3dmag.py --max-epochs $epochs \
     --batch-size $batch_size \
